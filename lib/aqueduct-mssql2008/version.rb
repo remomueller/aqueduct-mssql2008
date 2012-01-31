222ce743ed629cd3c14e3c14e44bdeab42b23676
Remo Mueller <remosm@gmail.com>
2012-01-31 08:00:04 -0500

    Version bump to 0.1.0.pre

--- lib/aqueduct-mssql2008/version.rb
+++ lib/aqueduct-mssql2008/version.rb
@@ -2,9 +2,9 @@ module Aqueduct
   module Mssql2008
     module VERSION
       MAJOR = 0
-      MINOR = 0
+      MINOR = 1
       TINY = 0
-      BUILD = nil # nil, "pre", "rc", "rc2"
+      BUILD = "pre" # nil, "pre", "rc", "rc2"
 
       STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
     end
